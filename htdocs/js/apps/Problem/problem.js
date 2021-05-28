22b55934d63d7dc7f063f69167590a7bee815e28
drdrew42 <kparker@citytech.cuny.edu>
2021-05-28 14:54:46 -0500

    revert problem.js to match ww-2.16

--- htdocs/js/apps/Problem/problem.js
+++ htdocs/js/apps/Problem/problem.js
@@ -1,19 +1,12 @@
 // Cause achievement popups to appear and then go away 
 $(window).on("load", function() {
-	$('#achievementModal').modal('show');
-	setTimeout(function() { $('#achievementModal').modal('hide'); }, 5000);
+    $('#achievementModal').modal('show');
+    setTimeout(function() { $('#achievementModal').modal('hide'); }, 60000);
 });
 
 $(function() {    
-    // Cause achievement popups to appear and then go away
-    $(window).load(function() { $('#achievementModal').modal('show');
-				setTimeout(function(){$('#achievementModal').modal('hide');},120000);
-			      });
-    
     // Prevent problems which are disabled from acting as links
     $('.problem-list .disabled-problem').parent().addClass('disabled')
-	.click(function (e) {
-	    e.preventDefault();
-	});
-    
+        .click(function (e) { e.preventDefault(); });
 })
+
